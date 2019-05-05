bcf695b767ee0c42d398f849d12e5f90c4655eb2
hrithik <hrithik.katiyar@lji.io>
2019-05-05 18:21:31 +0530

    Added patch, put, delete, post methods to our API

--- profiles/views.py
+++ profiles/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
+from profiles import serializers
 
 
 class HelloApiView(APIView):
     """Test APT View."""
+
+    serializer_class = serializers.HelloSerializer
 
     def get(self, request, fromat=None):
         """Returns a list of APIView features"""
@@ -18,4 +22,28 @@ class HelloApiView(APIView):
 
         return Response({'message': 'Hello', 'an_apiview': an_apiview})
 
+    def post(self, request):
+        """Create a hello message with a name."""
 
+        serializer = serializers.HelloSerializer(data=request.data)
+        if serializer.is_valid():
+            name = serializer.data.get("name")
+            message = 'Hello {0}'.format(name)
+            return Response({'message': message})
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def put(self, request, pk=None):
+        """Handles updating an object."""
+
+        return Response({'method': 'put'})
+
+    def patch(self, request, pk=None):
+        """Handles updates fields provided of an object"""
+
+        return Response({'method': 'patch'})
+
+    def delete(self, request, pk=None):
+        """Deletes an object"""
+
+        return Response({'method': 'delete'})
